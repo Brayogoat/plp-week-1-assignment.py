3adbd32d1759465472d49273693150daab59c032
Brayogoat <159621730+Brayogoat@users.noreply.github.com>
2024-03-10 18:03:36 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where do you live? ")
+message = f"Hello, {name}! You are {age} years old and you live in {location}. Nice to meet you!"
+print(message)
+
+
+
+
